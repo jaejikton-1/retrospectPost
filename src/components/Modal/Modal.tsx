1f07e78d84aba6ccf2fb7f8f24dbd9abd8bb4fbe
minsu.rob <minsu.rob@gmail.com>
2023-02-25 04:57:29 +0900

    fix: 모달창 레이아웃 변경

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Background, ModalCloseButton } from "./styles";
 import { useRef } from "react";
 import styled from "styled-components";
+import answerImg from "../../assets/AnswerBack.svg";
 
 const TimeTitle = () => {
   const todayTime = () => {
@@ -33,19 +34,41 @@ const Modal = ({ closeModal, children }: any) => {
         }
       }}
     >
-      <ModalLayout>
-        <TimeTitleLayout>
-          <TimeTitle />
-        </TimeTitleLayout>
-        {/* <ModalCloseButton onClick={() => closeModal(false)} /> */}
-        {children}
-        {/* <ModalButton onClick={() => closeModal(true)}>{buttonText}</ModalButton> */}{" "}
-      </ModalLayout>
+    <AnswerSentenceWrapper>
+      <AnswerImg src={answerImg}></AnswerImg>
+      <SentenceInput placeholder="답변을 입력해주세요"></SentenceInput>
+    </AnswerSentenceWrapper>
     </Background>
   );
 };
 
 export default Modal;
+const AnswerSentenceWrapper = styled.div`
+  position: relative;
+  margin-top: 70px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 290px;
+`;
+
+const AnswerImg = styled.img`
+  width: 100%;
+`;
+
+const SentenceInput = styled.textarea`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 30px;
+  background: none;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 const TimeTitleLayout = styled.div`
   position: absolute;
@@ -53,39 +76,4 @@ const TimeTitleLayout = styled.div`
   top: 3px;
   font-size: 12px;
   line-height: 17px;
-`;
-
-
-const ModalLayout = styled.div`
-  z-index: 999;
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-
-  /* max-height: 50%; */
-  width: 219px;
-  height: 435px;
-  background: white;
-  border-radius: 5px;
-  text-align: center;
-
-  display: flex;
-  flex-direction: column;
-
-  box-sizing: border-box;
-  border: 3px solid #a4d6cb;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-
-  font-weight: 400;
-  font-size: 15px;
-  line-height: 22px;
-
-  h1 {
-    margin-top: 30px;
-    margin-bottom: 28px;
-    font-weight: 700;
-    font-size: 20px;
-    line-height: 29px;
-  }
 `;
