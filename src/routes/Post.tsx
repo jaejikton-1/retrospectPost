e909fb54c5fe463ce2e298c53cb4ed97ae06a4a7
minsu.rob <minsu.rob@gmail.com>
2023-02-25 03:20:56 +0900

    etc: 임시 데이터 추가

--- src/routes/Post.tsx
+++ src/routes/Post.tsx
@@ -2,17 +2,16 @@ import { useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 //import {dbService, storageService} from "fbase";
 import styled from "styled-components";
-import React from 'react'
-
+import React from "react";
 type UserType = {
-    id: number,
-    name: string,
-    image : string,
-}
+  id: number;
+  name: string;
+  image: string;
+};
 
+//const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 ,22 , 23, 24, 25, 26, 27, 28];
 const Post = ({ userObj }: any) => {
-      
-    const dataA: Array<UserType> = [
+  const dataA: Array<UserType> = [
     {
       id: 1,
       name: "Island",
@@ -66,95 +65,120 @@ const Post = ({ userObj }: any) => {
       name: "Bridge",
       image:
         "https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05",
-    }
-    ,{
-      id: 1,
+    },
+    {
+      id: 10,
       name: "Island",
       image:
         "https://images.unsplash.com/photo-1442530792250-81629236fe54?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9631adb2d2f752e3a0734f393fef634b",
     },
     {
-      id: 2,
+      id: 11,
       name: "Forest",
       image:
         "https://images.unsplash.com/photo-1468851508491-4f854ec88aa0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=b1222b6a1d3694cac76d2a23c3a02254",
     },
     {
-      id: 3,
+      id: 12,
       name: "Whale",
       image:
         "https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=3c55430f01fe9ac9a9ccb3383d1416ff",
     },
     {
-      id: 4,
+      id: 13,
       name: "Mountain",
       image:
         "https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0",
     },
     {
-      id: 5,
+      id: 14,
       name: "Boat",
       image:
         "https://images.unsplash.com/photo-1443302382600-0bfacc473376?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=0c0f26518c1001f67b6c2e4480a8d3e0",
     },
     {
-      id: 6,
+      id: 15,
       name: "Flowers",
       image:
         "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc",
     },
     {
-      id: 7,
+      id: 16,
+      name: "Flowers",
+      image:
+        "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc",
+    },
+    {
+      id: 17,
       name: "Fire",
       image:
         "https://images.unsplash.com/photo-1468245856972-a0333f3f8293?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=1f57cc13084e32839627453821a43abf",
-    },{
-      id: 1,
+    },
+    {
+      id: 18,
       name: "Island",
       image:
         "https://images.unsplash.com/photo-1442530792250-81629236fe54?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9631adb2d2f752e3a0734f393fef634b",
     },
     {
-      id: 2,
+      id: 19,
       name: "Forest",
       image:
         "https://images.unsplash.com/photo-1468851508491-4f854ec88aa0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=b1222b6a1d3694cac76d2a23c3a02254",
     },
     {
-      id: 3,
+      id: 20,
       name: "Whale",
       image:
         "https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=3c55430f01fe9ac9a9ccb3383d1416ff",
     },
     {
-      id: 4,
+      id: 21,
       name: "Mountain",
       image:
         "https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0",
     },
     {
-      id: 5,
+      id: 22,
       name: "Boat",
       image:
         "https://images.unsplash.com/photo-1443302382600-0bfacc473376?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=0c0f26518c1001f67b6c2e4480a8d3e0",
     },
     {
-      id: 6,
+      id: 23,
       name: "Flowers",
       image:
         "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc",
     },
     {
-      id: 7,
+      id: 24,
       name: "Fire",
       image:
         "https://images.unsplash.com/photo-1468245856972-a0333f3f8293?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=1f57cc13084e32839627453821a43abf",
     },
     {
-      id: 6,
+      id: 25,
       name: "Flowers",
       image:
         "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc",
+    },
+    {
+      id: 26,
+      name: "Whale",
+      image:
+        "https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=3c55430f01fe9ac9a9ccb3383d1416ff",
+    },
+    {
+      id: 27,
+      name: "Mountain",
+      image:
+        "https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0",
+    },
+    {
+      id: 28,
+      name: "Fire",
+      image:
+        "https://images.unsplash.com/photo-1468245856972-a0333f3f8293?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=1f57cc13084e32839627453821a43abf",
     },
   ];
 
