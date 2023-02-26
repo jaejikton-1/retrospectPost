0b4ed336fc18be217b6f0af00c3bca9cfc1f5ad3
minsu.rob <minsu.rob@gmail.com>
2023-02-26 16:45:28 +0900

    feat: 메인 홈 이동 버튼 추가

---
+++ src/components/Modal/LetterModal.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Background, ModalCloseButton } from "./styles";
+import { useRef } from "react";
+import styled from "styled-components";
+import answerImg from "../../assets/AnswerBack.svg";
+import CloseButton from "../../assets/CloseButton.svg";
+import { useNavigate } from "react-router";
+
+const TimeTitle = () => {
+  const todayTime = () => {
+    let now = new Date();
+    let todayYear = now.getFullYear();
+    let todayMonth = now.getMonth() + 1;
+    let todayDate = now.getDate();
+
+    return todayYear + "." + todayMonth + "." + todayDate;
+  };
+
+  return (
+    <div>
+      {todayTime().slice(0, 9)}
+      <span>{todayTime().slice(9, 12)}</span>
+      <span>{todayTime().slice(12, 19)}</span>
+    </div>
+  );
+};
+
+const Modal = ({ closeModal, children }: any) => {
+  const outside: any = useRef();
+  const divRef = React.useRef<HTMLDivElement | null>(null)
+
+  const history = useNavigate();
+  const toPost = () => {
+    history("/question/1");
+  };
+
+  return (
+    <Background
+    ref={divRef}
+    onClick={ (e) => { if(e.target == outside.current) closeModal(false) } }
+    >
+    
+    <ModalWrapper>
+    <StampLayout src={CloseButton} onClick={closeModal}/>
+      <ModalImg onClick={toPost} src={answerImg}></ModalImg>
+    </ModalWrapper>
+    </Background>
+  );
+};
+
+export default Modal;
+
+const ModalWrapper = styled.div`
+  position: relative;
+  margin-top: 300px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 290px;
+`;
+
+const ModalImg = styled.img`
+  width: 100%;
+`;
+
+const SentenceInput = styled.textarea`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 30px;
+  background: none;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+
+const StampLayout = styled.img`
+  position: absolute;
+  width: 42px;
+  height: 42px;
+  left: 260.5px;
+  bottom: 140px
+`
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,17 +4,18 @@ import { useNavigate } from "react-router-dom";
 import { Container, TitleDiv, ButtonLayout } from "./styles/style";
 import { useEffect, useState } from "react";
 import { RedButton } from "./styles/buttonstyle";
-import Modal from "../components/Modal/Modal";
+import LetterModal from "../components/Modal/LetterModal";
+
 import styled from "styled-components";
 
 import Post from "../assets/Post.svg";
+import OKButtonImg from "../assets/OKButton.svg";
+import LetterButton from "../assets/LetterButton.svg";
 import React from "react";
 
 const Home = ({ userObj }: any) => {
   const [nweet, setNweet] = useState("");
   const [attachment, setAttachment] = useState("");
-
-
 
   const history = useNavigate();
   const toPost = () => {
@@ -24,16 +25,29 @@ const Home = ({ userObj }: any) => {
     history("/question/1");
   };
 
+  const [isModalOpen, setModalOpen] = useState(false);
 
+  const onClickOpenModal = () => {
+    setModalOpen(true);
+  };
+  const onClickCloseModal = () => {
+    setModalOpen((prev) => !prev);
+  };
 
   return (
     <>
       <Container>
         <TitleDiv>회고</TitleDiv>
         <PostImg src={Post} />
-        <RedButton onClick={toWrite}>회고 보내기</RedButton>
-        <RedButton onClick={toPost}>회고 보기</RedButton>
-        {" "}
+        <Text>
+          고영희 집배원이 <br />
+          <b>3통</b>의 편지를 가져왔어요!
+        </Text>
+        <SubText>떠나기 전에 답장해볼까요?</SubText>
+        <OKButton src={OKButtonImg} onClick={onClickOpenModal}/>
+        {isModalOpen && (
+          <LetterModal closeModal={onClickCloseModal}/>
+        )}{" "}
       </Container>
       {/* <div>
               {
@@ -51,8 +65,36 @@ const Home = ({ userObj }: any) => {
 
 export default Home;
 
+const OKButton = styled.img`
+  width: 60px;
+  height: 58px;
+  margin-top: 36px;
+`;
+const LetterImg = styled.img`
+  width: 76px;
+  height: 71px;
+  margin-top: 36px;
+`;
+
 const PostImg = styled.img`
   width: 180px;
   height: 285px;
   margin-top: 36px;
+`;
+
+
+const Text = styled.div`
+  font-size: 20px;
+  text-align: center;
+  line-height: 28px;
+
+  b {
+    color: #ff89a9;
+  }
+`;
+
+const SubText = styled.div`
+  font-size: 16px;
+  color: #494545;
+  margin-top: 20px;
 `;
